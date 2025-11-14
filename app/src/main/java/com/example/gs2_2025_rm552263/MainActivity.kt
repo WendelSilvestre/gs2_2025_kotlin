471d47ad84a251860eadee019852cb3aa4eb7e8a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-13 22:28:05 -0300

    Commit all

---
+++ app/src/main/java/com/example/gs2_2025_rm552263/MainActivity.kt
@@ -0,0 +1,44 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.gs2_2025_rm552263.ui.theme.Android5navigationbetweenscreensTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android5navigationbetweenscreensTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "equipe") {
+                            EquipeScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gs2_2025_rm552263/screens/LoginScreen.kt
@@ -0,0 +1,115 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier, navController: NavController) {
+
+    var usuario = remember { mutableStateOf("") }
+    var senha = remember { mutableStateOf("") }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF2C4EC7))
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "LOGIN",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(
+                text = "Usuário:",
+                modifier = Modifier.padding(bottom = 8.dp),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = usuario.value,
+                onValueChange = { usuario.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(text = "Insira seu usuário.")
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color.Red,
+                    focusedBorderColor = Color.Red
+                ),
+                shape = RoundedCornerShape(16.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Senha:",
+                modifier = Modifier.padding(bottom = 8.dp),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = senha.value,
+                onValueChange = { senha.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Insira sua senha."
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color.Red,
+                    focusedBorderColor = Color.Red
+                ),
+                shape = RoundedCornerShape(16.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType =
+                        KeyboardType.Text
+                )
+            )
+        }
+        Button(
+            onClick = {
+                if (usuario.value == "admin" && senha.value == "123456") {
+                    navController.navigate("menu")
+                } else {
+                    navController.navigate("login")
+                }
+            },
+            colors = ButtonDefaults.buttonColors(Color.White),
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(
+                text = "Entrar",
+                fontSize = 20.sp,
+                color = Color.Blue
+            )
+        }
+    }
+}
